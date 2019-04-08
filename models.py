a2b51fa77082c715aa201d5edadc50e3b20245c6
kishansairam9 <kishansairam.adapa9@gmail.com>
2019-04-08 18:30:16 +0000

    Removed extra braces

--- models.py
+++ models.py
@@ -19,8 +19,8 @@ class Quiz(db.Model):
 
     id = db.Column('id', db.Integer, primary_key=True)
     date_added = db.Column(DateTime, default=datetime.datetime.now)
-    sub_q1 = db.Column('q1', db.String(10)))
-    sub_q2 = db.Column('q2', db.String(10)))
-    sub_q3 = db.Column('q3', db.String(10)))
-    sub_q4 = db.Column('q4', db.String(10)))
-    sub_q5 = db.Column('q5', db.String(120))) # Fix max length when implementing flash method in views.py    
+    sub_q1 = db.Column('q1', db.String(10))
+    sub_q2 = db.Column('q2', db.String(10))
+    sub_q3 = db.Column('q3', db.String(10))
+    sub_q4 = db.Column('q4', db.String(10))
+    sub_q5 = db.Column('q5', db.String(120)) # Fix max length when implementing flash method in views.py    
