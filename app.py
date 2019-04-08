23779fa547b9550207fa60f63a65ad9f9a42ad71
ista2000 <mishra.istasis@gmail.com>
2019-04-08 16:57:09 +0530

    Integrated API for RSA key generation, message encryption and decryption

--- app.py
+++ app.py
@@ -6,8 +6,5 @@ app = Flask(__name__)
 
 app.register_blueprint(main)
 
-#current = int(os.environ.get('PORT', 5000))
-
 if __name__ == '__main__':
     app.run()
-    #  app.run(host='0.0.0.0', port=current)
